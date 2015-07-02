be5b625b1edae7f506ddc71ffe8ba8655d3ba67c
Jared <jardavies89@gmail.com>
2015-07-02 15:10:55 -0400

    Have most the elements on the filter page now.  Will stop here to get feedback on how to progress at our next meeting.

--- src/main/webapp/ui/views/filter/FilterController.js
+++ src/main/webapp/ui/views/filter/FilterController.js
@@ -1,11 +1,25 @@
 angular.module('foodbankfarm')
-    .controller('FilterController', [
-        '$scope',
-        'LocationRepository',
-        function ($scope) {
-            $scope.responsible = {
-                name: 'Jared',
-                email: 'jardavies89@gmail.com'
-            };
-        }
-    ]);
+    .controller('FilterController', ['$scope','LocationRepository', function ($scope) {
+
+    	
+    	$scope.shouldShowSpecialtyProducts = false;
+    	
+    	$scope.checkboxModel = {
+    			allField : false,
+        		specialtyProductField : false,
+        		farmOutletField : false,
+        		csaField : false,
+        		farmersMarketField : false,
+        		retailOutletField : false,
+        		foodBankPartnerField : false,
+        		acceptSNAPField : false,
+        		honeyField : false,
+        		treeFarmField : false,
+        		milkAndIceCreamField : false,
+        		cheeseField : false,
+        		mushroomField : false,
+        		wineField : false
+        };
+        	
+    }]);
+
