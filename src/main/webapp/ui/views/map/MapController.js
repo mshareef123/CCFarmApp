09882e025c2aeca8bc8edaf5f2a4a6f609de59e0
eaziz <abdulaziz.ergashev@gmail.com>
2015-07-13 05:19:40 -0400

    Added back accidentally reverted (by Jared) changes - showing popup on info on map bubble click

--- src/main/webapp/ui/views/map/MapController.js
+++ src/main/webapp/ui/views/map/MapController.js
@@ -11,20 +11,41 @@ angular.module('foodbankfarm.map', ['uiGmapgoogle-maps'])
 	        $scope.map = {center: {latitude: 40.0010204, longitude: -75.8069082 }, zoom: 10 };// chester county long lat
 	        $scope.options = {scrollwheel: false};
 	        $scope.farmmarkers = [];
+	        
+	        var createMarker = function (i, locations,idKey) {
+                var ret = {
+            	        latitude:  locations[i].longitude,
+            	        longitude:  locations[i].latitude,
+            	        title:  locations[i].farmName,
+                        show: false,
+            	      };
+              ret.onClick = function() {
+                  ret.show = !ret.show;
+	              window.console.log("Clicked!");
+
+              };
+              ret[idKey] = i;
+              return ret;
+	        };
+	        
 	        $scope.$watch(function() {
 	            return $scope.map.bounds;
 	          }, function(nv, ov) {
 	            // Only need to regenerate once
 	              var markers = [];
 	              for (var i = 0; i < $scope.locations.length; i++) {
-	                  var ret = {
-	                	        latitude:  $scope.locations[i].longitude,
-	                	        longitude:  $scope.locations[i].latitude,
-	                	        title:  $scope.locations[i].farmName,
-	                	        id:i
-	                	      };
-
-	                markers.push(ret);
+//	                  var ret = {
+//	                	        latitude:  $scope.locations[i].longitude,
+//	                	        longitude:  $scope.locations[i].latitude,
+//	                	        title:  $scope.locations[i].farmName,
+//	                            show: false,
+//	                	      };
+//	                  ret.onClick = function() {
+//	                      ret.show = !ret.show;
+//	                  };
+//	                  ret["id"] = i;
+//
+	                markers.push(createMarker(i, $scope.locations,'id'));
 	              }
 	              $scope.farmmarkers = markers;
 	          }, true);
