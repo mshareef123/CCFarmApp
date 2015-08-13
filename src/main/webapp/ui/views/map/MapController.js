562dda1a17386f0bb238a7128a866dd96c20e294
eaziz <IandR@10.195.57.58>
2015-08-13 17:05:40 -0400

    Fixed map search is working

--- src/main/webapp/ui/views/map/MapController.js
+++ src/main/webapp/ui/views/map/MapController.js
@@ -1,40 +1,74 @@
 angular.module('foodbankfarm.map', ['uiGmapgoogle-maps'])
     .controller('MapController', 
     		['$rootScope',
+    		 '$location',
              '$scope',
-             function($rootScope,$scope) {
-              $scope.shouldShowFilter = false;
-    		
-//            LocationRepository.list('queryString').then(function (result) {
-//                $scope.locations = result.data;
-//            });
-
+             function($rootScope,$location,$scope) {
+    		//$scope.locations = $rootScope.filteredLocations
+            $scope.shouldShowFilter = false;
 	        $scope.map = {center: {latitude: 40.0010204, longitude: -75.8069082 }, zoom: 10 };// chester county long lat
 	        $scope.options = {scrollwheel: false};
-	        
-	        var events = {
-	                places_changed: function (searchBox) {}
-	              }
-	              $scope.searchbox = { template:'searchbox.tpl.html', events:events};
-
-	        
 	        $scope.farmmarkers = [];
 	        
 	        var createMarker = function (i, locations,idKey) {
+	        	var specialitiesStr = '';
+	        	if(locations[i].specialities && locations[i].specialities.length >0){
+	        		specialitiesStr = locations[i].specialities.join(", ");
+	        	}
+	        	
                 var ret = {
             	        latitude:  locations[i].latitude,
             	        longitude:  locations[i].longitude,
             	        title:  locations[i].farmName,
+            	        address:  locations[i].address,
+            	        specialities : specialitiesStr,
                         show: false,
             	      };
               ret.onClick = function() {
+
                   ret.show = !ret.show;
 	              window.console.log("Clicked!");
 
               };
+              ret.viewDetail  = function(){
+                	 $location.path('/detail/' + i);
+                };
               ret[idKey] = i;
               return ret;
 	        };
+	        
+	        var events = {
+	                places_changed: function (searchBox) {
+	                	var places=searchBox.getPlaces();
+
+	                    if (places.length == 0) {
+	                      return;
+	                    }
+	                	
+	                	var newMarkers = $scope.locations;
+	                    var bounds = new google.maps.LatLngBounds();
+	                    for (var i = 0, place; place = places[i]; i++) {
+	                      // Create a marker for each place.
+	                      var marker = {
+	                        id:i,
+	                        place_id: place.place_id,
+	                        title: 'Search Location!!!',
+	                        latitude: place.geometry.location.lat(),
+	                        longitude: place.geometry.location.lng(),
+	                        address:place.adr_address
+	                        //show: false
+	                        //templateurl:'window.tpl.html',
+	                       // templateparameter: place
+	                      };
+	                      $scope.map = {center: {latitude: places[0].geometry.location.lat(), longitude: places[0].geometry.location.lng() }, zoom: 13 };
+		                    
+	                      newMarkers.push(marker);
+
+	                      bounds.extend(place.geometry.location);
+	                    }
+	                }
+	              }
+	        $scope.searchbox = { template:'searchbox.tpl.html', events:events,position: 'top-left'};
 	        
 	        $scope.$watch(function() {
 	            return $scope.map.bounds;
@@ -42,20 +76,13 @@ angular.module('foodbankfarm.map', ['uiGmapgoogle-maps'])
 	            // Only need to regenerate once
 	              var markers = [];
 	              for (var i = 0; i < $rootScope.filteredLocations.length; i++) {
-//	                  var ret = {
-//	                	        latitude:  $scope.locations[i].longitude,
-//	                	        longitude:  $scope.locations[i].latitude,
-//	                	        title:  $scope.locations[i].farmName,
-//	                            show: false,
-//	                	      };
-//	                  ret.onClick = function() {
-//	                      ret.show = !ret.show;
-//	                  };
-//	                  ret["id"] = i;
-//
 	                markers.push(createMarker(i, $rootScope.filteredLocations,'id'));
 	              }
 	              $scope.farmmarkers = markers;
 	          }, true);
+	        
+	        $scope.viewDetail  = function(id){
+           	 $location.path('/detail/' + id);
+           };
 
     }]);
