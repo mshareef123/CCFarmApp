dc75a8d2ffc9489940f19722097bc61945f12ab0
eaziz <abdulaziz.ergashev@gmail.com>
2015-08-16 13:59:17 -0400

    Added logo image

--- src/main/webapp/ui/js/repositories/LocationRepository.js
+++ src/main/webapp/ui/js/repositories/LocationRepository.js
@@ -3,6 +3,8 @@
         '$http',
         function ($http) {
             var serverUrl = 'http://localhost:8080/controller';
+            //var serverUrl = 'http://52.24.208.79/foodbankfarm/controller';//server upload
+
             return {
 					list: function (query) {
 						Require.IsNotNull(query, 'query');
