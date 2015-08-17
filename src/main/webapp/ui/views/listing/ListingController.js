a565f291db88d3dab5de4c39d35e65d5e4880ad6
eaziz <IandR@10.195.57.58>
2015-08-17 15:03:44 -0400

    Work on the sorting for distance

--- src/main/webapp/ui/views/listing/ListingController.js
+++ src/main/webapp/ui/views/listing/ListingController.js
@@ -29,36 +29,5 @@ angular.module('foodbankfarm')
             };
         
         
-        var events = {
-                places_changed: function (searchBox) {
-                	var places=searchBox.getPlaces();
 
-                    if (places.length == 0) {
-                      return;
-                    }
-                	
-                	var newMarkers = $scope.locations;
-                    var bounds = new google.maps.LatLngBounds();
-                    for (var i = 0, place; place = places[i]; i++) {
-                      // Create a marker for each place.
-                      var marker = {
-                        id:i,
-                        place_id: place.place_id,
-                        title: 'Search Location!!!',
-                        latitude: place.geometry.location.lat(),
-                        longitude: place.geometry.location.lng(),
-                        address:place.adr_address
-                        //show: false
-                        //templateurl:'window.tpl.html',
-                       // templateparameter: place
-                      };
-                      $scope.map = {center: {latitude: places[0].geometry.location.lat(), longitude: places[0].geometry.location.lng() }, zoom: 13 };
-	                    
-                      newMarkers.push(marker);
-
-                      bounds.extend(place.geometry.location);
-                    }
-                }
-              }
-        $scope.searchbox = { template:'searchbox.tpl.html', events:events};
 }]);
--- src/main/webapp/ui/views/map/MapController.js
+++ src/main/webapp/ui/views/map/MapController.js
@@ -88,4 +88,46 @@ angular.module('foodbankfarm.map', ['uiGmapgoogle-maps'])
            	 $location.path('/detail/' + id);
            };
 
+           
+           //This is all my sort by distance stuff
+           $scope.shouldShowDistanceSearchbar = false;
+
+           $scope.valueChanged = function(radioButtonValue) {
+               if(radioButtonValue == 'Distance') {
+               	$scope.shouldShowDistanceSearchbar = true;
+               	$scope.setUpSearchBox();
+               } else {
+               	$scope.shouldShowDistanceSearchbar = false;
+               }
+           };
+           
+           
+           $scope.setUpSearchBox = function () {
+        	   var input = document.getElementById('sortByDistanceField');
+               $scope.searchBox = new google.maps.places.SearchBox(input);
+               
+               // Listen for the event fired when the user selects a prediction and retrieve
+               // more details for that place.
+               $scope.searchBox.addListener('places_changed', function() {
+            	   var places = $scope.searchBox.getPlaces();
+  
+            	   if (places.length == 0) {
+            		   return;
+            	   } else {
+            		   $rootScope.currentSortLocation = places[0];
+            	   }
+            	   
+            	   var numberOfLocations = $rootScope.locations.length;
+            	   for(var i = 0; i < numberOfLocations; i++) {
+            		   var currentLocation = $rootScope.locations[i];
+            		   var currentLocationLatLng = new google.maps.LatLng(currentLocation.latitude, currentLocation.longitude);
+            		   var sortLocationLatLng = new google.maps.LatLng($rootScope.currentSortLocation.geometry.location.lat(), $rootScope.currentSortLocation.geometry.location.lng());
+            		   var distance = distance = google.maps.geometry.spherical.computeDistanceBetween(currentLocationLatLng, sortLocationLatLng)
+            		   currentLocation.sortDistance = distance;
+            		   console.log(currentLocation.sortDistance);
+            	   }
+            	   
+            	   
+               });
+           }
     }]);
