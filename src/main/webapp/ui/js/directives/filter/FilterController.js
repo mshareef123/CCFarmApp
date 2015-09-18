f638a82369085b5fbe4595b3f1b51b0bd85ed6be
eaziz <IandR@innovations-mbp.home>
2015-09-18 10:28:56 -0400

    Added the new filter types, and updated listing and details to reflect it.

--- src/main/webapp/ui/js/directives/filter/FilterController.js
+++ src/main/webapp/ui/js/directives/filter/FilterController.js
@@ -1,75 +1,101 @@
 angular.module('foodbankfarm') 
     .controller('FilterController', ['$scope','$rootScope', function ($scope,$rootScope) {
-    	
-    	$scope.shouldShowSpecialtyProducts = false;
-   		$scope.allCheckBoxSelected = false;     
-   		$scope.allSpecialitySelected = false;
-   		
+   
     	//Setup our categories so that we can build them the UI page.
+
     	$rootScope.getCategoriesFromLocation = function () {
     		var products = [];
-    		var specialities = [];
+    		var typesOfOpp = [];
+    		var specialDesignation = [];
+    		
     		for(var i = 0; i < $rootScope.locations.length; i++) {
        			var currentFarm = $rootScope.locations[i];
        			products = _.union(products, currentFarm.products);  			
-       			specialities = _.union(specialities, currentFarm.specialities);
+       			typesOfOpp = _.union(typesOfOpp, currentFarm.typeOfOperation);
+       			specialDesignation = _.union(specialDesignation, currentFarm.designation);
        		}  
     
-    		$rootScope.filterCheckBoxes = [];
-    		$rootScope.specialityCheckBoxes = [];
+    		$rootScope.productCheckBoxes = [];
+    		$rootScope.oppCheckBoxes = [];
+    		$rootScope.designationCheckBoxes = [];
     		
         	for (var i = 0; i < products.length; i++) {
         		var productLabel = products[i];
         		if(productLabel.length > 0) {
         			var checkBoxObject = {label : productLabel, isSelected: false};
-        			$rootScope.filterCheckBoxes.push(checkBoxObject);
+        			$rootScope.productCheckBoxes.push(checkBoxObject);
         		}
         	}
         	
-        	for(var i = 0; i < specialities.length; i++) {
-        		var specialtyLabel = specialities[i];
-        		if(specialtyLabel.length > 0) {
-        			var checkBoxObject = {label : specialities[i] , isSelected: false};
-        			$rootScope.specialityCheckBoxes.push(checkBoxObject);
+        	for(var i = 0; i < typesOfOpp.length; i++) {
+        		var oppLabel = typesOfOpp[i];
+        		if(oppLabel.length > 0) {
+        			var checkBoxObject = {label : oppLabel , isSelected: false};
+        			$rootScope.oppCheckBoxes.push(checkBoxObject);
+        		}
+        	}
+        	
+        	for(var i = 0; i < specialDesignation.length; i++) {
+        		var designationLabel = specialDesignation[i];
+        		if(designationLabel.length > 0) {
+        			var checkBoxObject = {label : designationLabel , isSelected: false};
+        			$rootScope.designationCheckBoxes.push(checkBoxObject);
         		}
         	}
     	};  
     	
-    	$scope.resetFilters = function() {
-    		for(var i = 0; i < $rootScope.filterCheckBoxes.length; i++) {
-    			var checkBoxObject = $rootScope.filterCheckBoxes[i];
+    	var resetFiltersForAllBoxes = function() {
+    		for(var i = 0; i < $rootScope.productCheckBoxes.length; i++) {
+    			var checkBoxObject = $rootScope.productCheckBoxes[i];
     			checkBoxObject.isSelected = false;
     		}
     		
-    		for(var i = 0; i < $rootScope.specialityCheckBoxes.length; i++) {
-    			var checkBoxObject = $rootScope.specialityCheckBoxes[i];
+    		for(var i = 0; i < $rootScope.oppCheckBoxes.length; i++) {
+    			var checkBoxObject = $rootScope.oppCheckBoxes[i];
+    			checkBoxObject.isSelected = false;
+    		}
+    		
+    		for(var i = 0; i < $rootScope.designationCheckBoxes.length; i++) {
+    			var checkBoxObject = $rootScope.designationCheckBoxes[i];
     			checkBoxObject.isSelected = false;
     		}
     	}
     	
+    	$scope.resetFilters = function() {
+    		//I'm hoping this helps the map update.
+    		resetFiltersForAllBoxes();
+    	}
+    	
     	$scope.checkBoxChanged = function () {
-    		var allProductFilters = angular.copy($rootScope.filterCheckBoxes);
-    		var allSpecialityFilters = angular.copy($rootScope.specialityCheckBoxes);
-    		
-    		var currentProductFilters = _.remove(allProductFilters, function(filter) {
-    			return filter.isSelected == true;
-    		}); 
-    		
-    		var currentSpecialityFilters = _.remove(allSpecialityFilters, function(filter) {
-    			return filter.isSelected == true;
-    		}); 
-    		
-    		//Okay now we need to just get the labels...probably a better way to do this, couldn't think of one at the moment so this'll do for now.
+    		var allProductFilters = angular.copy($rootScope.productCheckBoxes);
+    		var allOppFilters = angular.copy($rootScope.oppCheckBoxes);
+    		var allDesignationFilters = angular.copy($rootScope.designationCheckBoxes);
     		var filterLabels = [];
     		
-    		for(var i = 0; i < currentProductFilters.length; i++) {
-    			var currentFilterLabel = currentProductFilters[i].label;
-    			filterLabels.push(currentFilterLabel);
+    		//filter out all the not selected ones, and get the labels
+    		
+    		for(var i = 0; i < allProductFilters.length; i++) {
+    			var currentBox = allProductFilters[i];
+    			if(currentBox.isSelected) {
+    				var currentFilterLabel = currentBox.label;
+    				filterLabels.push(currentFilterLabel);
+    			}
     		}
     		
-    		for(var i = 0; i < currentSpecialityFilters.length; i++) {
-    			var currentFilterLabel = currentSpecialityFilters[i].label;
-    			filterLabels.push(currentFilterLabel);
+    		for(var i = 0; i < allOppFilters.length; i++) {
+    			var currentBox = allOppFilters[i];
+    			if(currentBox.isSelected) {
+    				var currentFilterLabel = currentBox.label;
+    				filterLabels.push(currentFilterLabel);
+    			}
+    		}
+    		
+    		for(var i = 0; i < allDesignationFilters.length; i++) {
+    			var currentBox = allDesignationFilters[i];
+    			if(currentBox.isSelected) {
+    				var currentFilterLabel = currentBox.label;
+    				filterLabels.push(currentFilterLabel);
+    			}
     		}
     		
     		//Our actual filtering is rather simple - we will just intersect the specialties on each product with our filter labels, and if the intersection
@@ -77,20 +103,19 @@ angular.module('foodbankfarm')
     		var filteredLocations = [];
     		for(var i = 0; i < $rootScope.locations.length; i++) {
     			var currentLocation = $rootScope.locations[i];
+    			
     			var intersectedProducts = _.intersection(currentLocation.products, filterLabels)
-    			var intersectedSpecialities = _.intersection(currentLocation.specialities, filterLabels);
-    			var totalFilters = intersectedProducts.length + intersectedSpecialities.length;
+    			var intersectedOpp = _.intersection(currentLocation.typeOfOperation, filterLabels);
+    			var intersectedDesignation = _.intersection(currentLocation.designation, filterLabels);
+    			
+    			var totalFilters = intersectedProducts.length + intersectedOpp.length + intersectedDesignation.length;
+    			
     			if(totalFilters == filterLabels.length) {
     				filteredLocations.push(currentLocation);
     			}
     		}
-    		
-    		//This little check is for the condition when you turned off all the filters and we need to go back to basic state
-    		if(currentProductFilters.length != 0 || currentSpecialityFilters.length != 0) {
-    			$rootScope.filteredLocations = filteredLocations;
-    		} else {
-    			$rootScope.filteredLocations = angular.copy($rootScope.locations);
-    		}
+
+    		$rootScope.filteredLocations = filteredLocations;
 
     	};
     	
